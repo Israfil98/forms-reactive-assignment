63b33f8d904e035e694adb52302dcb61ebfeb06f
Israfil98 <israfil.guseynov.98@inbox.ru>
2022-07-25 17:57:58 +0400

    complete the assignment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
+import { Observable } from "rxjs";
 
 @Component({
   selector: "app-root",
@@ -6,5 +8,43 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./app.component.css"],
 })
 export class AppComponent implements OnInit {
-  ngOnInit(): void {}
+  projectForm: FormGroup;
+
+  forbiddenProjectNames = ["Test"];
+
+  ngOnInit(): void {
+    this.projectForm = new FormGroup({
+      projectName: new FormControl(null, [
+        Validators.required,
+        this.forbiddenProject.bind(this),
+      ]),
+      email: new FormControl(null, [Validators.required, Validators.email], this.forbiddenEmailAsync),
+      status: new FormControl("stable", Validators.required),
+    });
+  }
+
+  forbiddenProject(control: FormControl): { [s: string]: boolean } {
+    if (this.forbiddenProjectNames.indexOf(control.value) !== -1) {
+      return { invalidProjectName: true };
+    }
+
+    return null;
+  }
+
+  forbiddenEmailAsync(control: FormControl): Promise<any> | Observable<any> {
+    const promise = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value === "test@gmail.com") {
+          resolve({ invalidProjectName: true });
+        }
+        resolve(null);
+      }, 1000);
+    });
+
+    return promise;
+  }
+
+  onSubmit() {
+    console.log(this.projectForm);
+  }
 }
